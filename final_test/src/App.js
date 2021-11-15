0bb08b7fe1a103b1c9051d268e17ea81f520f409
duong <tovanduonghp1996@gmail.com>
2021-11-15 21:23:03 +0700

    add new

---
+++ final_test/src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+import Main from './component/mainComponent'
+function App() {
+  return (
+    <div className="App">
+        <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ final_test/src/component/mainComponent.js
@@ -0,0 +1,9 @@
+import react from "react";
+
+function Main () {
+    return (
+        <h1>Main</h1>
+    )
+}
+
+export default Main;
